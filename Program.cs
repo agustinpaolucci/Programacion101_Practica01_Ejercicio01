edd5af60464c2f7c1c8ea41906617b8d4ed40f97
agustinpaolucci <globalprint@gmail.com>
2023-05-14 13:13:19 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion101_Practica01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rec01 = new Rectangulo();
+
+            Console.WriteLine("Ingrese la altura del rectánguilo en cms...");
+            rec01.Alto = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la anchura del rectángulo en cms...");
+            rec01.Ancho = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("El area del rectangulo de medidas " + rec01.ToString() + " es: ");
+
+            Console.WriteLine(rec01.CacularArea(rec01.Alto, rec01.Ancho)+" cm3.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangulo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion101_Practica01
+{
+    public class Rectangulo
+    {
+        private double ancho;
+        private double alto;
+
+        public double Ancho
+        {
+            get { return ancho; }
+            set { ancho = value; }
+        }
+
+        public double Alto
+        {
+            get { return alto; }
+            set { alto = value; }
+        }
+
+        public Rectangulo()
+        {
+            ancho = 0.0;
+            alto = 0.0;
+        }
+
+        public Rectangulo(double ancho, double alto)
+        {
+            this.ancho = ancho;
+            this.alto = alto;
+        }
+
+        public double CacularArea(double ancho, double alto)
+        {
+            double area = 0.0;
+            area = ancho * alto;
+            return area;
+        }
+
+        public override string ToString()
+        {
+            return "alto: "+this.alto+ " y ancho: " + this.ancho;
+        }
+
+    }
+}
